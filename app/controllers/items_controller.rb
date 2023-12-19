2bb2a75234b83595351790f333461286db753f64
miracletea <takewotatekaketatomo@gmail.com>
2023-12-20 01:25:55 +0900

    itemsコントローラの9-13行目コメントアウト
    app/controllers/items_controller.rb
    Comment on lines +9 to +13
      def move_to_index
        unless user_signed_in?
          redirect_to action: :index
        end
      end

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,9 @@ class ItemsController < ApplicationController
   
 
   private
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  # def move_to_index
+    # unless user_signed_in?
+      # redirect_to action: :index
+    # end
+  # end
 end
