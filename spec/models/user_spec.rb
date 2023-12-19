d3a7af62afec4028c4c04eebd0a9f404ff4355f0
miracletea <takewotatekaketatomo@gmail.com>
2023-12-20 02:50:32 +0900

    異常系テストの追加
    以下の異常系テストを追加しましょう。
    ■パスワード
    ・英字のみのパスワードでは登録できない
    ・数字のみのパスワードでは登録できない
    ・全角文字を含むパスワードでは登録できない
    ↑パスワードのテストは半角英数字混合での入力でなければ登録できないを確かめるために、上記のように細分化してテストを行いましょう。
    
    ■姓・名（全角）
    ・姓（全角）に半角文字が含まれていると登録できない
    ・名（全角）に半角文字が含まれていると登録できない
    
    ■姓・名（カナ）
    ・姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない
    ・名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,6 +88,85 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
+
+      it ' 英字のみのパスワードでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it ' 全角文字を含むパスワードでは登録できない' do
+        @user.password = 'Ａ12345'
+        @user.password_confirmation = 'Ａ12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = 'ﾔﾏﾀﾞ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = 'ﾀﾛｳ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+      it 'last_name_kanaにカタカナ以外の文字（平仮名）が含まれていると登録できない' do
+        @user.last_name_kana = 'やまだ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it 'last_name_kanaにカタカナ以外の文字（漢字）が含まれていると登録できない' do
+        @user.last_name_kana = '山田'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it 'last_name_kanaにカタカナ以外の文字（英字）が含まれていると登録できない' do
+        @user.last_name_kana = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it 'last_name_kanaにカタカナ以外の文字（数字）が含まれていると登録できない' do
+        @user.last_name_kana = '123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it 'last_name_kanaにカタカナ以外の文字（記号）が含まれていると登録できない' do
+        @user.last_name_kana = '@'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it 'first_name_kanaカタカナ以外の文字（平仮名）が含まれていると登録できない' do
+        @user.first_name_kana = 'たろう'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it 'first_name_kanaカタカナ以外の文字（漢字）が含まれていると登録できない' do
+        @user.first_name_kana = '太郎'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it 'first_name_kanaカタカナ以外の文字（英字）が含まれていると登録できない' do
+        @user.first_name_kana = 'tarou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it 'first_name_kanaカタカナ以外の文字（数字）が含まれていると登録できない' do
+        @user.first_name_kana = '123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it 'first_name_kanaカタカナ以外の文字（記号）が含まれていると登録できない' do
+        @user.first_name_kana = '@'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
     end
   end
 end
