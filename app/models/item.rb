79b250ecc1a2b507a76445131dcab1d11256eac2
miracletea <takewotatekaketatomo@gmail.com>
2023-12-23 16:16:24 +0900

    only_integer: trueの見直し
    priceにオプションを付ける

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,14 @@ class Item < ApplicationRecord
 
   validate :image_attached
   validates :name, :detail,presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :category_id, presence: true
   validates :condition_id, presence: true
   validates :shipping_charge_id, presence: true
   validates :ship_from_to_id, presence: true
   validates :days_to_ship_date_id, presence: true
 
-  validates :category_id, :condition_id, :shipping_charge_id, :ship_from_to_id, :days_to_ship_date_id, numericality: { only_integer: true, other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id, :shipping_charge_id, :ship_from_to_id, :days_to_ship_date_id, numericality: {other_than: 1 , message: "can't be blank"}
   
   def image_attached
     unless image.attached?
