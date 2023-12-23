2dddc2ed25428fd00f7337691a7a05a1b80c78d7
miracletea <takewotatekaketatomo@gmail.com>
2023-12-23 15:48:57 +0900

    only_integer:trueのオプションの追加
    app/models/item.rb
    
      validate :image_attached
      validates :name, :detail,presence: true
      validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
    @koyamatakumi koyamatakumi 7 minutes ago
      validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
    only_integer:trueのオプションを追加しましょう。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     shipping_charge_id    {Faker::Number.between(from: 2, to:4)}
     ship_from_to_id       {Faker::Number.between(from: 2, to:48)}
     days_to_ship_date_id  {Faker::Number.between(from: 2, to:3)}
-    price                 {Faker::Number.between(from: 300, to:9999999)}
+    price                 {Faker::Number.between(from: 300, to:9999999, only_integer: true)}
     association :user
 
     after(:build) do |item|
