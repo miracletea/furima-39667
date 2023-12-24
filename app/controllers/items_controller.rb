45195f044a5a288a91d285e6945563dd56c618d5
miracletea <takewotatekaketatomo@gmail.com>
2023-12-24 16:06:36 +0900

    updateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except:[:index, :show]
+  before_action :move_to_new_user_session, except:[:index, :show, :new]
   before_action :authenticate_user!, except:[:index, :show]
 
   def index
@@ -27,13 +27,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
-  # def move_to_index
-    # unless user_signed_in?
-      # redirect_to action: :index
-    # end
-  # end
+  def move_to_new_user_session
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to new_user_session_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(:image, :name, :detail, :category_id, :condition_id, :shipping_charge_id, :ship_from_to_id, :days_to_ship_date_id, :price).merge(user_id: current_user.id)
