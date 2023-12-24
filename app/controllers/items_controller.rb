6142c1ca117e3ccf9bbfe5512c564eb47c4f6f74
miracletea <takewotatekaketatomo@gmail.com>
2023-12-24 20:51:53 +0900

    【任意修正】set_itemのmove_to_pageを削除
    app/controllers/items_controller.rb
    Comment on lines +2 to 4
      before_action :set_item, only: [:show, :edit, :update, :move_to_page, :destroy]
      before_action :move_to_page, except:[:index, :show, :new, :create]
      before_action :authenticate_user!, except:[:index, :show]
    @div-erika div-erika 8 minutes ago
    こちらは【任意修正】となります。
    よりよいコードを書くための知識としてご確認いただけますと幸いです。
    
      before_action :set_item, only: [:show, :edit, :update, :move_to_page, :destroy]
    before_action :set_itemからmove_to_pageは削除していただいても大丈夫です。
    
      before_action :set_item, only: [:show, :edit, :update, :move_to_page, :destroy]
      before_action :move_to_page, except:[:index, :show, :new, :create]
      before_action :authenticate_user!, except:[:index, :show]
    理由：
    上記の記述によって、set_itemの次にmove_to_pageが動くようになっています。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :move_to_page, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_page, except:[:index, :show, :new, :create]
   before_action :authenticate_user!, except:[:index, :show]
 
