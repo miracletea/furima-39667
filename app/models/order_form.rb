63dc1c61c13d0bd5c44c862ca8d69d8c3be70748
miracletea <takewotatekaketatomo@gmail.com>
2023-12-28 14:29:03 +0900

    presence:trueをwith_optionに変更

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,14 +2,17 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :token, :user_id, :item_id, :postal_code, :ship_from_to_id, :city_town_village, :street_address, :building_name, :phone_number
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :token, presence: true
-  validates :postal_code, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/ }
-  validates :ship_from_to_id, presence: true
-  validates :city_town_village, presence: true
-  validates :street_address, presence: true
-  validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/ }
+  with_options presence: true do
+  validates :user_id
+  validates :item_id
+  validates :token
+  validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
+  validates :ship_from_to_id
+  validates :city_town_village
+  validates :street_address
+  validates :phone_number, format: {with: /\A\d{10,11}\z/ }
+  end
+
 
   validates :ship_from_to_id, numericality: {other_than: 1 , message: "can't be blank"}
 
