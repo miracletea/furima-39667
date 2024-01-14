c292dfe7a48142a4b2e8f0a6ca835d304100d5a5
miracletea <takewotatekaketatomo@gmail.com>
2024-01-14 16:09:23 +0900

    turbo:load イベントの追加
    Turboフレームがロードされたときに発生する turbo:load イベントをリッスンしているJavaScriptの一部を示しています。このイベントは、Turboフレームが新しいページを読み込む際にトリガーされます。
    
    通常、このイベントはTurboフレームを使用している場合に特に役立ちます。Turboフレームは、ページの一部だけを更新してページの再読み込みを行わずに新しいコンテンツを表示するため、通常のページ遷移よりも高速です。
    
    以下は、提供されたコードの続きやイベントハンドラの中身などがあれば教えていただければ、もっと具体的なアドバイスを提供できるかもしれません。どのように続くかによって、Turboフレームを使用してページの読み込み後に実行される特定の処理が異なるからです。

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,30 +1,33 @@
 import consumer from "./consumer"
 
-if(location.pathname.match(/\/items\/\d/)){
+document.addEventListener("turbo:load", function() {
+  
+  if(location.pathname.match(/\/items\/\d/)){
 
-  consumer.subscriptions.create({
-    channel: "CommentChannel",
-    item_id: location.pathname.match(/\d+/)[0]
-  }, {
+    consumer.subscriptions.create({
+      channel: "CommentChannel",
+      item_id: location.pathname.match(/\d+/)[0]
+    }, {
 
-    connected() {
-      // Called when the subscription is ready for use on the server
-    },
+      connected() {
+        // Called when the subscription is ready for use on the server
+      },
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-    received(data) {
-      const html = `
-        <div class="comment">
-          <p class="user-info">${data.user.nickname}: </p>
-          <p>${data.comment.text}</p>
-        </div>`
-      const comments = document.getElementById("comments")
-      comments.insertAdjacentHTML('beforeend', html)
-      const commentForm = document.getElementById("comment-form")
-      commentForm.reset();
-    }
-  })
-}
+      received(data) {
+        const html = `
+          <div class="comment">
+            <p class="user-info">${data.user.nickname}: </p>
+            <p>${data.comment.text}</p>
+          </div>`
+        const comments = document.getElementById("comments")
+        comments.insertAdjacentHTML('beforeend', html)
+        const commentForm = document.getElementById("comment-form")
+        commentForm.reset();
+      }
+    })
+  }
+})
